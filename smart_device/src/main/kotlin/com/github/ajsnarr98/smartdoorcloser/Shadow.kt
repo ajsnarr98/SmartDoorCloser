62cab130234721f9f3efba1093ecc4300930825f
AJ Snarr <ajsnarr98@gmail.com>
2021-01-16 16:10:05 -0500

    Added thing name as shadow property

--- smart_device/src/main/kotlin/com/github/ajsnarr98/smartdoorcloser/Shadow.kt
+++ smart_device/src/main/kotlin/com/github/ajsnarr98/smartdoorcloser/Shadow.kt
@@ -8,6 +8,7 @@ import kotlin.concurrent.withLock
  * Represents this IoT device's shadow.
  *
  * @property id - device id
+ * @property thingName - the thingName for this device
  * @property friendlyName - human-readable name of device (en-US)
  * @property lastCloseCmd - the unix-epoch-based time of the last command to
  *                          close the door
@@ -18,6 +19,7 @@ import kotlin.concurrent.withLock
  */
 data class Shadow(
     var id: String,
+    var thingName: String,
     var friendlyName: String,
     var lastCloseCmd: String,
     var closeCmdCompleted: String,
@@ -52,6 +54,7 @@ data class Shadow(
         if (map == null) return
 
         id = map[ID_PROPERTY] as? String ?: this.id
+        thingName = map[THING_NAME_PROPERTY] as? String ?: this.thingName
         friendlyName = map[FRIENDLY_NAME_PROPERTY] as? String ?: this.friendlyName
         lastCloseCmd = map[LAST_CLOSE_CMD_PROPERTY] as? String ?: this.lastCloseCmd
         closeCmdCompleted = map[CLOSE_CMD_COMPLETED_PROPERTY] as? String ?: this.closeCmdCompleted
@@ -66,6 +69,7 @@ data class Shadow(
     fun asHashMap(excludeLastCloseCmd: Boolean = false): HashMap<String, Any?> {
         return HashMap<String, Any?>().apply {
             put(ID_PROPERTY, id)
+            put(THING_NAME_PROPERTY, thingName)
             put(FRIENDLY_NAME_PROPERTY, friendlyName)
             if (!excludeLastCloseCmd) put(LAST_CLOSE_CMD_PROPERTY, lastCloseCmd)
             put(CLOSE_CMD_COMPLETED_PROPERTY, closeCmdCompleted)
@@ -75,6 +79,7 @@ data class Shadow(
     companion object {
         fun buildFrom(config: Config, map: Map<String, Any>?): Shadow {
             val id: String = config.thingName ?: throw IllegalStateException("thingName should not be null")
+            val thingName: String = config.thingName ?: throw IllegalStateException("thingName should not be null")
             val friendlyName: String = map?.get(FRIENDLY_NAME_PROPERTY) as? String ?: FRIENDLY_NAME_DEFAULT
             var lastCloseCmd: String = map?.get(LAST_CLOSE_CMD_PROPERTY) as? String ?: CLOSE_CMD_DEFAULT
             val closeCmdCompleted: String = map?.get(CLOSE_CMD_COMPLETED_PROPERTY) as? String ?: lastCloseCmd
@@ -84,12 +89,14 @@ data class Shadow(
             }
             return Shadow(
                 id = id,
+                thingName = thingName,
                 friendlyName = friendlyName,
                 lastCloseCmd = lastCloseCmd,
                 closeCmdCompleted = closeCmdCompleted,
             ).apply {
                 needsUpdate = map == null
                     || id != map[ID_PROPERTY]
+                    || thingName != map[THING_NAME_PROPERTY]
                     || friendlyName != map[FRIENDLY_NAME_PROPERTY]
                     || lastCloseCmd != map[LAST_CLOSE_CMD_PROPERTY]
                     || closeCmdCompleted != map[CLOSE_CMD_COMPLETED_PROPERTY]
@@ -97,6 +104,7 @@ data class Shadow(
         }
 
         private const val ID_PROPERTY = "id"
+        private const val THING_NAME_PROPERTY = "thingName"
         private const val FRIENDLY_NAME_PROPERTY = "friendly_name"
         private const val LAST_CLOSE_CMD_PROPERTY = "last_close_cmd"
         private const val CLOSE_CMD_COMPLETED_PROPERTY = "close_cmd_completed"
